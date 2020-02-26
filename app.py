c86d50ed5e7bc23914811f56b82a63c31cbd35c6
Ed <edmundocall.eoc@gmail.com>
2020-02-26 14:28:01 +0000

    added a gitignore file, for env.py variable, and added templates

--- app.py
+++ app.py
@@ -3,21 +3,33 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+from os import path
+if path.exists("env.py"):
+  import env 
+
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'motorcycleReviews'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@eoccluster-jcwib.mongodb.net/motorcycleReviews?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.environ.get('motorcycleURI')
 
 
 mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/get_tasks')
-def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+def index():
+    return render_template("index.html")
 
 
-@app.route('/add_task')
-def add_task():
-    return render_template('critic-reviews.html', categories=mongo.db.categories.find())
+@app.route('/critic_reviews')
+def critic_reviews():
+    return render_template('critic-reviews.html')
+
+
+@app.route('/user_reviews')
+def user_reviews():
+    return render_template("user-reviews.html")
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
