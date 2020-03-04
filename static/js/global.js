af982406e8c897e2420dbc67ebdec75100a592ad
Ed <edmundocall.eoc@gmail.com>
2020-03-04 09:22:29 +0000

    changed padding and margins around reviews for critics and users and added font and box shadow

--- static/js/global.js
+++ static/js/global.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
-	  if (scroll > 300) {
-	    $(".black").css("background" , "blue");
-	  }
-
-	  else{
-		  $(".black").css("background" , "#333");  	
-	  }
+	  if (scroll > 1) {
+	    $("#navbar-scroll").removeClass('navbar-scroll');
+      }
+      
   })
 })
+
+// $(function () {
+//             $(document).scroll(function () {
+//                 var $nav = $("#mainNavbar");
+//                 $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+//             });
+//         });
