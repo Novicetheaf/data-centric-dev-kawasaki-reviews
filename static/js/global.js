ae0bf0a6d7cf25b585a7fee4754f69e9172190ff
Ed <edmundocall.eoc@gmail.com>
2020-03-04 09:52:43 +0000

    added navbar scroll font change

--- static/js/global.js
+++ static/js/global.js
@@ -1,16 +1,16 @@
+// Scrolled navbar coloring
 $(document).ready(function(){
-  $(window).scroll(function(){
-  	var scroll = $(window).scrollTop();
-	  if (scroll > 1) {
-	    $("#navbar-scroll").removeClass('navbar-scroll');
-      }
-      
-  })
+
+    $(function () {
+
+        $(document).scroll(function () {
+
+            const navbar = $("#navbar-scroll");
+            navbar.toggleClass("scrolled", $(this).scrollTop() > navbar.height());
+           
+        });
+
+    });
+
 })
 
-// $(function () {
-//             $(document).scroll(function () {
-//                 var $nav = $("#mainNavbar");
-//                 $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-//             });
-//         });
