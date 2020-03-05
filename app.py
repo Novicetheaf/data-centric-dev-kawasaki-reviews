144128d8f62623f21aa0c0b3603ebd3251059f03
Ed <edmundocall.eoc@gmail.com>
2020-03-05 11:19:46 +0000

    added show more option for critic reviews

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ app.config["MONGO_URI"] = os.environ.get('motorcycleURI')
 mongo = PyMongo(app)
 
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -28,16 +29,16 @@ def critic_reviews_search():
 
 @app.route('/user_reviews')
 def user_reviews():
-    return render_template("user-reviews.html", userReview=mongo.db.ownerReviews.find())
+    return render_template("user-reviews.html",
+    userReview=mongo.db.ownerReviews.find())
 
 
 # Sort motorcycles by model
 
-@app.route('/h2r_model')
-def h2r_model():
+@app.route('/show_more/<review_id>')
+def show_more(review_id):
     return render_template('filter-critic-motorcycle-reviews.html',
-                           siteReview=mongo.db.siteReview.find
-                           ({'model': 'Ninja H2R'}))
+    siteReview=mongo.db.siteReview.find({'_id': ObjectId(review_id)}))
 
 
 @app.route('/vulcan_model')
@@ -65,6 +66,13 @@ def add_review():
 def insert_review():
     userReview = mongo.db.ownerReviews
 
+    h2r_image = "https://raw.githubusercontent.com/Novicetheaf/data-centric-dev-kawasaki-reviews/master/static/images/2018-Kawasaki-Ninja-H2R1.jpg"
+    vulcan_s_image = "https://raw.githubusercontent.com/Novicetheaf/data-centric-dev-kawasaki-reviews/master/static/images/2017-Kawasaki-Vulcan-S-ABS-Cafe-Review-5.jpg"
+    versys_image = "https://raw.githubusercontent.com/Novicetheaf/data-centric-dev-kawasaki-reviews/master/static/images/2019-kawasaki-versys-1000-se-lt-corner.jpg"
+    zx6r_image = "https://raw.githubusercontent.com/Novicetheaf/data-centric-dev-kawasaki-reviews/master/static/images/20_ZX6R-krt-hero1-scaled.jpeg"
+    klr_650_image = "https://raw.githubusercontent.com/Novicetheaf/data-centric-dev-kawasaki-reviews/master/static/images/KawasakiKLR6502016_060-768x512.jpg"
+    z900_image = "https://raw.githubusercontent.com/Novicetheaf/data-centric-dev-kawasaki-reviews/master/static/images/g-000221-g_W2210131_11-kawasaki-z900-636982140121390902.jpg"
+
     model_select = request.form['model_select']
     overall_rating = request.form['overall_rating']
     name = request.form['name']
@@ -75,6 +83,24 @@ def insert_review():
     running_costs_and_value = request.form['running_costs_and_value']
     review_summary = request.form['review_summary']
 
+    if model_select == "H2R":
+        image = h2r_image
+
+    elif model_select == "Vulcan S":
+        image = vulcan_s_image
+   
+    elif model_select == "KLR 650":
+        image = klr_650_image
+
+    elif model_select == "Z900":
+        image = z900_image
+
+    elif model_select == "Versys 1000":
+        image = versys_image
+
+    elif model_select == "ZX-6R":
+        image = zx6r_image
+
     review_form = {
         'model_select': model_select,
         'overall_rating': overall_rating,
@@ -83,7 +109,8 @@ def insert_review():
         'engine': engine,
         'build_quality_and_reliability': build_quality_and_reliability,
         'running_costs_and_value': running_costs_and_value,
-        'review_summary': review_summary
+        'review_summary': review_summary,
+        'image': image
     }
 
     userReview.insert_one(review_form)
