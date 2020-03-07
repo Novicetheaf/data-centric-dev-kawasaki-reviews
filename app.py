270681b79c0a1269904387c36b3cbcba4c7de6d5
Ed <edmundocall.eoc@gmail.com>
2020-03-07 12:14:49 +0000

    set debug mode to false

--- app.py
+++ app.py
@@ -175,4 +175,4 @@ def update_review(review_id):
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(
-        os.environ.get('PORT')), debug=True)
+        os.environ.get('PORT')), debug=False)
